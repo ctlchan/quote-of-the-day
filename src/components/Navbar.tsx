a829689dfefdb59590bfbd5f25c73b1e0b16398c
ctlchan <christakleungchan@gmail.com>
2025-08-10 14:00:31 -0400

    Add image and switch to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,21 @@
+import Image from "next/image";
+import DarkModeToggle from "./DarkModeToggle";
+import quote from '@/assets/quote.png'
+
 export default function Navbar() {
     return (
-        <div className="flex justify-center top-0 p-2.5 bg-gray-200 opacity-70">
-            <h1 className="text-2xl">
+        <div className="flex justify-between items-center top-0 p-2.5 bg-gray-200 opacity-70">
+            <Image 
+                src={quote} 
+                alt="Quote Icon"
+                width={50}
+                height={50}
+                className="ml-3"
+            />
+            <h1 className="text-2xl font-bold drop-shadow-gray-800 drop-shadow-2xl">
                 Quote of the Day
             </h1>
+            <DarkModeToggle/>
         </div>
     )
 }
