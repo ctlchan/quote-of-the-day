916a772330c807afa2297bdf27a6205f8cbfdd12
ctlchan <christakleungchan@gmail.com>
2025-08-10 14:01:00 -0400

    Update attribution

--- src/components/Attribution.tsx
+++ src/components/Attribution.tsx
@@ -2,7 +2,10 @@ export default function Attribution() {
     return (
         <div className="flex justify-center bottom-0">
             <p>
-                Inspirational quotes provided by <a href="htpps://zenquotes.io/" target="_blank" className="text-blue-300 hover:shadow">ZenQuotes API</a>
+                Inspirational quotes provided by&nbsp;
+                    <a href="htpps://zenquotes.io/" target="_blank" className="text-blue-300 hover:shadow">ZenQuotes API</a>. 
+                &nbsp;Utterance icons created by Freepik -&nbsp;
+                    <a href="https://www.flaticon.com/free-icons/utterance" title="utterance icons" className="text-blue-300 hover:shadow">Flaticon</a>
             </p>
         </div>
     )
